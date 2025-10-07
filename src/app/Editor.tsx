f884402ffddd089be50c81053af9a19ed99b3c4f
vivid-image <vivid23780757@gmail.com>
2025-10-07 18:09:27 +0800

    20251007一切的開始~

---
+++ src/app/Editor.tsx
@@ -0,0 +1,54 @@
+import EditorJS, {API, BlockMutationEvent} from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
+
+const editor = new EditorJS({
+    // 預設為唯獨
+    readOnly: true,
+
+    // 開始後解除唯獨模式
+    onReady: () => {
+        console.log('聊天室已經準備好囉!!')
+        editor.readOnly.toggle();
+    },
+
+    // 編輯器的ID
+    holder: 'editorjs',
+
+    //預設內容
+    placeholder: '提醒：請不要撰寫任何色情、暴力、政治相關的內容，官方有權刪除不當內容',
+
+    // 工具配置
+    // 傳入每個工具的類別或物件
+    tools: {
+        header: {
+            class: Header,
+            inlineToolbar: true
+        },
+    },
+
+    onChange(api: API, event: BlockMutationEvent | BlockMutationEvent[]) {
+        console.log('你編輯了：', event);
+    }
+
+    data: {}
+});
+
+// promise寫法
+// editor.isReady
+//     .then(()=>{
+//         console.log('Editor已經準備好了')
+//     })
+//     .catch((reason)=>{
+//         console.log(`Editor準備失敗，原因：${reason}`)
+//     })
+
+// async/await寫法
+try {
+    await editor.isReady;
+    console.log('Editor已經準備好了')
+} catch (reason) {
+    console.log(`Editor準備失敗，原因：${reason}`)
+}
+
+
